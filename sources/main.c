0eb951a3d74dd21b326cddd9d153b0203bfd8493
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-09 19:47:13 +0200

    [add](sources/main.c) Add gameboy `entrypoint`

---
+++ sources/main.c
@@ -0,0 +1,9 @@
+#include "../include/gameboy.h"
+
+int main(int argc, char **argv)
+{
+    GameboyClass *gameboy = new_class(Gameboy);
+    int exit_code = gameboy->run(gameboy, argc, argv);
+    destroy_class(gameboy);   
+    return exit_code;
+}
