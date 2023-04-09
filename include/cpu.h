67feaa627020eedae28a0dfc0158cdf4f6cf403d
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-09 21:46:33 +0200

    [add](include/cpu.h) Add `cpu` class methods declarations

--- include/cpu.h
+++ include/cpu.h
@@ -1,3 +1,6 @@
+#include "bus.h"
+#include "common.h"
+#include "instructions.h"
 #include "oop.h"
 
 #ifndef __CPU
@@ -8,9 +11,19 @@ typedef struct cpu_aux CPUClass;
 typedef struct cpu_aux {
     /* Properties */
     class_t metadata;
+    cpu_context_t *context;
+    BusClass *bus;
+    InstructionsClass *instructions;
     /* Methods */
     void (*init)(CPUClass *);
     bool (*step)(CPUClass *);
+    void (*fetch_instructions)(CPUClass *);
+    void (*fetch_data)(CPUClass *);
+    void (*execute)(CPUClass *);
+    void (*set_flags)(CPUClass *, char, char, char, char);
+    bool (*check_condition)(CPUClass *);
+    uint16_t (*reverse)(uint16_t);
+    uint16_t (*read_register)(CPUClass *, register_type_t);
 } CPUClass;
 
 extern const class_t *CPU;
