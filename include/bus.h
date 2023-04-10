f2a4e6c7d08752f15980df75ce27152b45e4bbd1
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-10 18:48:23 +0200

    [add](include/bus.h) Add `ram` class property

--- include/bus.h
+++ include/bus.h
@@ -1,15 +1,20 @@
 #include "cartridge.h"
 #include "common.h"
 #include "oop.h"
+#include "ram.h"
 
 #ifndef __BUS
     #define __BUS
+
 typedef struct bus_aux BusClass;
+typedef struct gameboy_aux GameboyClass;
 
 typedef struct bus_aux {
     /* Properties */
     class_t metadata;
     CartridgeClass *cartridge;
+    RamClass *ram;
+    GameboyClass *parent;
     /* Methods */
     uint8_t (*read)(BusClass *, uint16_t);
     void (*write)(BusClass *, uint16_t, uint8_t);
