d3201a751950ba76f5670590241e08801c04c54f
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-10 16:39:03 +0200

    [add](sources/bus.c) Add `switch` statement with `address` ranges,`read16` and `write16` method definitions

--- sources/bus.c
+++ sources/bus.c
@@ -8,19 +8,63 @@ static void constructor(void *ptr, va_list *args)
 
 static uint8_t read(BusClass *self, uint16_t address)
 {
-    if (address < 0x8000) {
-        return self->cartridge->read(self->cartridge, address);
+    switch (address) {
+        case 0 ... 0x7FFF: {
+            return self->cartridge->read(self->cartridge, address);
+        }
+        case 0x8000 ... 0x9FFF: HANDLE_ERROR("not implemented read");
+        case 0xA000 ... 0xBFFF: HANDLE_ERROR("not implemented read");
+        case 0xC000 ... 0xDFFF: HANDLE_ERROR("not implemented read");
+        case 0xE000 ... 0xFDFF: HANDLE_ERROR("not implemented read");
+        case 0xFE00 ... 0xFE9F: HANDLE_ERROR("not implemented read");
+        case 0xFEA0 ... 0xFEFF: HANDLE_ERROR("not implemented read");
+        case 0xFF00 ... 0xFF7F: HANDLE_ERROR("not implemented read");
+        case 0xFF80 ... 0xFFFE: HANDLE_ERROR("not implemented read");
+        case 0xFFFF: HANDLE_ERROR("not implemented read");
+        default: {
+            char buff[256];
+            sprintf(buff, "not implemented read %04X", address);
+            HANDLE_ERROR(buff);
+        }
     }
-    HANDLE_ERROR("not implemented");
 }
 
 static void write(BusClass *self, uint16_t address, uint8_t value)
 {
-    if (address < 0x8000) {
-        self->cartridge->write(self->cartridge, address, value);
-        return;
+    switch (address) {
+        case 0 ... 0x7FFF: {
+            self->cartridge->write(self->cartridge, address, value);
+            break;
+        }
+        case 0x8000 ... 0x9FFF: HANDLE_ERROR("not implemented write");
+        case 0xA000 ... 0xBFFF: HANDLE_ERROR("not implemented write");
+        case 0xC000 ... 0xDFFF: HANDLE_ERROR("not implemented write");
+        case 0xE000 ... 0xFDFF: HANDLE_ERROR("not implemented write");
+        case 0xFE00 ... 0xFE9F: HANDLE_ERROR("not implemented write");
+        case 0xFEA0 ... 0xFEFF: HANDLE_ERROR("not implemented write");
+        case 0xFF00 ... 0xFF7F: HANDLE_ERROR("not implemented write");
+        case 0xFF80 ... 0xFFFE: HANDLE_ERROR("not implemented write");
+        case 0xFFFF: HANDLE_ERROR("not implemented write");
+        default: {
+            char buff[256];
+            sprintf(buff, "not implemented write %04X", address);
+            HANDLE_ERROR(buff);
+        }
     }
-    HANDLE_ERROR("not implemented");
+}
+
+static uint16_t read16(BusClass *self, uint16_t address)
+{
+    uint16_t lo = self->read(self, address);
+    uint16_t hi = self->read(self, address + 1);
+
+    return lo | (hi << 8);
+}
+
+static void write16(BusClass *self, uint16_t address, uint16_t value)
+{
+    self->write(self, address + 1, (value >> 8) & 0xFF);
+    self->write(self, address, value & 0xFF);
 }
 
 const BusClass bus_init = {
@@ -32,6 +76,8 @@ const BusClass bus_init = {
     },
     .read = read,
     .write = write,
+    .read16 = read16,
+    .write16 = write16,
 };
 
 const class_t *Bus = (const class_t *) &bus_init;
