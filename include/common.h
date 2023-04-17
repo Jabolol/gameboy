70bafd6c4194c5e6edb540599583c88243b120bf
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-04-17 11:54:26 +0200

    [add](include/common.h) Add `ansi` colors

--- include/common.h
+++ include/common.h
@@ -2,9 +2,26 @@
 
 #ifndef __COMMON
     #define __COMMON
-    #define ANSI_COLOR_RED    "\x1b[31m"
-    #define ANSI_COLOR_YELLOW "\x1b[33m"
-    #define ANSI_COLOR_RESET  "\x1b[0m"
+    #define ANSI_COLOR_BLACK          "\x1b[30m"
+    #define ANSI_COLOR_RED            "\x1b[31m"
+    #define ANSI_COLOR_GREEN          "\x1b[32m"
+    #define ANSI_COLOR_YELLOW         "\x1b[33m"
+    #define ANSI_COLOR_BLUE           "\x1b[34m"
+    #define ANSI_COLOR_MAGENTA        "\x1b[35m"
+    #define ANSI_COLOR_CYAN           "\x1b[36m"
+    #define ANSI_COLOR_WHITE          "\x1b[37m"
+    #define ANSI_COLOR_BRIGHT_BLACK   "\x1b[90m"
+    #define ANSI_COLOR_BRIGHT_RED     "\x1b[91m"
+    #define ANSI_COLOR_BRIGHT_GREEN   "\x1b[92m"
+    #define ANSI_COLOR_BRIGHT_YELLOW  "\x1b[93m"
+    #define ANSI_COLOR_BRIGHT_BLUE    "\x1b[94m"
+    #define ANSI_COLOR_BRIGHT_MAGENTA "\x1b[95m"
+    #define ANSI_COLOR_BRIGHT_CYAN    "\x1b[96m"
+    #define ANSI_COLOR_BRIGHT_WHITE   "\x1b[97m"
+    #define ANSI_COLOR_RESET          "\x1b[0m"
+    #define ANSI_COLOR_BOLD           "\x1b[1m"
+    #define ANSI_COLOR_UNDERLINE      "\x1b[4m"
+    #define ANSI_COLOR_BLINK          "\x1b[5m"
     #define HANDLE_ERROR(e)                                                 \
         do {                                                                \
             fprintf(stderr,                                                 \
