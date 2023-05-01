78a7b896961d128c22bad9f8baeeb86b46797b44
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-05-01 18:21:38 +0200

    [add](sources/gameboy.c) Run the `cpu` in a separate `thread`

--- sources/gameboy.c
+++ sources/gameboy.c
@@ -9,6 +9,7 @@ static void constructor(void *ptr, va_list UNUSED *args)
     self->bus = new_class(Bus, self->cartridge, self->ram, self);
     self->cpu = new_class(CPU, self->bus, self->instructions, self);
     self->stack = new_class(Stack, self->cpu);
+    self->ui = new_class(Ui, self, 1024, 768);
     if (!((self->context = calloc(1, sizeof(*self->context))))) {
         HANDLE_ERROR("failed memory allocation");
     }
@@ -23,30 +24,22 @@ static void destructor(void *ptr)
     destroy_class(self->instructions);
     destroy_class(self->ram);
     destroy_class(self->stack);
+    destroy_class(self->ui);
     free(self->context);
 }
 
-static int run(GameboyClass *self, int argc, char **argv)
+static void *cpu_run(void *ptr)
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: ./gameboy /path/to/rom.gb\n");
-        return 1;
-    }
-    if (!self->cartridge->load(self->cartridge, argv[1])) {
-        fprintf(stderr, "Failed to load ROM file: %s\n", argv[1]);
-        return 1;
-    }
-    printf("Cartridge successfully loaded\n");
-    SDL_Init(SDL_INIT_VIDEO);
-    printf("SDL initialized\n");
-    TTF_Init();
-    printf("TTF initialized\n");
-    self->cpu->init(self->cpu);
+    GameboyClass *self = (GameboyClass *) ptr;
+
     self->context->running = true;
     self->context->paused = false;
     self->context->ticks = 0;
 
     while (self->context->running) {
+        if (self->context->die) {
+            pthread_exit(NULL);
+        }
         if (self->context->paused) {
             SDL_Delay(10);
             continue;
@@ -57,6 +50,35 @@ static int run(GameboyClass *self, int argc, char **argv)
         }
         self->context->ticks += 1;
     }
+    return 0;
+}
+
+static int32_t run(GameboyClass *self, int argc, char **argv)
+{
+    pthread_t thread;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./gameboy /path/to/rom.gb\n");
+        return 1;
+    }
+
+    if (!self->cartridge->load(self->cartridge, argv[1])) {
+        fprintf(stderr, "Failed to load ROM file: %s\n", argv[1]);
+        return 1;
+    }
+
+    printf("Cartridge successfully loaded\n");
+
+    if (pthread_create(&thread, NULL, self->cpu_run, self) != 0) {
+        HANDLE_ERROR("Failed to create thread");
+    }
+
+    while (!self->context->die) {
+        nanosleep(&(struct timespec){.tv_nsec = 1000000}, NULL);
+        self->ui->handle_events(self->ui);
+    }
+
+    pthread_join(thread, NULL);
 
     return 0;
 }
@@ -76,6 +98,7 @@ const GameboyClass init_gameboy = {
     .cpu = NULL,
     .run = run,
     .cycles = cycles,
+    .cpu_run = cpu_run,
 };
 
 const class_t *Gameboy = (const class_t *) &init_gameboy;
