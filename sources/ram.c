35e53b9bfb57b852194ab67fed8d6d96c3c9c240
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-05-13 21:20:42 +0200

    [fix](sources/ram.c) Fix `spelling`

--- sources/ram.c
+++ sources/ram.c
@@ -2,7 +2,7 @@
 
 static void constructor(void *ptr, va_list UNUSED *args)
 {
-    RamClass *self = (RamClass *) ptr;
+    RAMClass *self = (RAMClass *) ptr;
     if (!((self->context = calloc(1, sizeof(*self->context))))) {
         HANDLE_ERROR("failed memory allocation");
     }
@@ -10,42 +10,41 @@ static void constructor(void *ptr, va_list UNUSED *args)
 
 static void destructor(void *ptr)
 {
-    RamClass *self = (RamClass *) ptr;
+    RAMClass *self = (RAMClass *) ptr;
     free(self->context);
 }
 
-static uint8_t wram_read(RamClass *self, uint16_t address)
+static uint8_t wram_read(RAMClass *self, uint16_t address)
 {
     return self->context->wram[address - 0xC000];
 }
 
-static void wram_write(RamClass *self, uint16_t address, uint8_t value)
+static void wram_write(RAMClass *self, uint16_t address, uint8_t value)
 {
     self->context->wram[address - 0xC000] = value;
 }
 
-static uint8_t hram_read(RamClass *self, uint16_t address)
+static uint8_t hram_read(RAMClass *self, uint16_t address)
 {
     return self->context->hram[address - 0xFF80];
 }
 
-static void hram_write(RamClass *self, uint16_t address, uint8_t value)
+static void hram_write(RAMClass *self, uint16_t address, uint8_t value)
 {
     self->context->hram[address - 0xFF80] = value;
 }
 
-const RamClass init_ram = {
+const RAMClass init_ram = {
     {
-        ._size = sizeof(RamClass),
-        ._name = "Ram",
+        ._size = sizeof(RAMClass),
+        ._name = "RAM",
         ._constructor = constructor,
         ._destructor = destructor,
     },
-    .context = NULL,
     .wram_read = wram_read,
     .wram_write = wram_write,
     .hram_read = hram_read,
     .hram_write = hram_write,
 };
 
-const class_t *Ram = (const class_t *) &init_ram;
+const class_t *RAM = (const class_t *) &init_ram;
