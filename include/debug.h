d05794aee1f1f91ef9c318b447349c55616af6f6
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-05-15 21:47:11 +0200

    [add](include/debug.h) Add `cpu_step` method

--- include/debug.h
+++ include/debug.h
@@ -12,10 +12,13 @@ typedef struct debug_aux {
     class_t metadata;
     GameboyClass *parent;
     char message[1024];
+    char buffer[BUFFER_SIZE];
+    char instruction_data[INST_BUFF_LEN];
     int32_t message_size;
     /* Methods */
     void (*update)(DebugClass *);
     void (*print)(DebugClass *);
+    void (*cpu_step)(DebugClass *, uint16_t);
 } DebugClass;
 
 extern const class_t *Debug;
