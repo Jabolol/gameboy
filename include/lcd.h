066ab7a56cd3f2d31faae7c58de3cb115239fc96
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-05-15 21:48:46 +0200

    [add](include/lcd.h) Create `LCD` class

---
+++ include/lcd.h
@@ -0,0 +1,23 @@
+#include "common.h"
+#include "oop.h"
+
+#ifndef __LCD
+    #define __LCD
+
+typedef struct gameboy_aux GameboyClass;
+typedef struct lcd_aux LCDClass;
+
+typedef struct lcd_aux {
+    /* Properties */
+    class_t metadata;
+    GameboyClass *parent;
+    uint64_t default_colors[4];
+    lcd_context_t *context;
+    /* Methods */
+    uint8_t (*read)(LCDClass *, uint16_t);
+    void (*write)(LCDClass *, uint16_t, uint8_t);
+    void (*update)(LCDClass *, uint8_t, uint8_t);
+} LCDClass;
+
+extern const class_t *LCD;
+#endif
