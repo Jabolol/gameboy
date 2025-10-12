af5c006f7d2710f69649a7ee6084cfcacab6852d
Javier R <javier.rios-urbano@epitech.eu>
2025-10-12 12:36:30 +0200

    chore: update layout and `logo` formatting

--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -2,7 +2,7 @@ import Canvas from "../islands/Canvas.tsx";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full h-screen flex-col gap-5">
+    <div className="flex items-center justify-center w-full min-h-screen bg-white dark:bg-black">
       <Canvas />
     </div>
   );
